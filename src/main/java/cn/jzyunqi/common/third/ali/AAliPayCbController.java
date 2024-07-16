4dc09bd6e75adde371e61a078f503041583b558a
DarkDance <57419153@qq.com>
2024-07-16 17:26:02 +0800

    支付处理

--- src/main/java/cn/jzyunqi/common/third/ali/AAliPayCbController.java
+++ src/main/java/cn/jzyunqi/common/third/ali/AAliPayCbController.java
@@ -1,7 +1,8 @@
 package cn.jzyunqi.common.third.ali;
 
 import cn.jzyunqi.common.exception.BusinessException;
-import cn.jzyunqi.common.third.ali.client.AliPayClient;
+import cn.jzyunqi.common.feature.pay.PayCallbackDto;
+import cn.jzyunqi.common.feature.pay.PayCallbackProcessor;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,12 @@ import java.util.Map;
  * 支付宝回调接口
  *
  * @author wiiyaya
- * @date 2021/5/9.
+ * @since 2021/5/9.
  */
-public abstract class AAliPayCbController {
+public abstract class AAliPayCbController implements PayCallbackProcessor {
 
     @Resource
-    private AliPayClient aliPayClient;
+    private AliPayStrange aliPayStrange;
 
     /**
      * 支付宝支付回调
@@ -33,22 +34,15 @@ public abstract class AAliPayCbController {
                                        @RequestParam(value = "out_trade_no", required = false) String applyPayNo,
                                        @RequestParam(value = "trade_no") String actualPayNo,
                                        @RequestParam(value = "price", required = false) BigDecimal actualPayAmount
-                                       ) throws BusinessException {
-        boolean checkResult = aliPayClient.payCallBackCheck(paramMap);
-        if(checkResult){
-            paySuccess(applyPayNo, actualPayNo, actualPayAmount, params);
-        }else{
-            throw new BusinessException("common_ali_pay_call_back_failed");
-        }
-    }
+    ) throws BusinessException {
+        PayCallbackDto payCallbackDto = new PayCallbackDto();
+        payCallbackDto.setApplyPayNo(applyPayNo);
+        payCallbackDto.setActualPayType("ali");
+        payCallbackDto.setActualPayNo(actualPayNo);
+        payCallbackDto.setActualPayAmount(actualPayAmount);
+        payCallbackDto.setReturnParamMap(paramMap);
+        payCallbackDto.setReturnParam(params);
 
-    /**
-     * 支付成功回调
-     *
-     * @param applyPayNo 申请支付订单号
-     * @param actualPayNo 实际支付订单号
-     * @param actualPayAmount 实际支付金额
-     * @param returnParam 返回参数
-     */
-    protected abstract void paySuccess(String applyPayNo, String actualPayNo, BigDecimal actualPayAmount, String returnParam);
+        verifyPayCallback(aliPayStrange, payCallbackDto);
+    }
 }
